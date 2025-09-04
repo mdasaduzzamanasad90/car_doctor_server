bee71f9b3f3c0bc16ef43bb257f1985c7f3d05bf
AH-Sazzad <mdasaduzzamanasad100@gmail.com>
2025-09-04 16:02:52 +0600

    server is working

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("car-doctor");
     const services = database.collection("services");
@@ -73,7 +73,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
